b8f0c20f5e5fa83c7b4049fc7ec7e70deed5e798
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2025-03-19 15:08:12 +0530

    trying to fix db issue

--- api/src/routes/payment.ts
+++ api/src/routes/payment.ts
@@ -1,14 +1,26 @@
-import express from 'express';
-import { createCheckoutSession, getBankDetails, addBankDetail, handlePaymentCallback, updateBankDetails, createPaymentIntent } from '../controllers/payment.controller';
-import { authMiddleware } from '../middleware/auth';
+import express from "express";
+import {
+  createCheckoutSession,
+  getBankDetails,
+  addBankDetail,
+  updateBankDetails,
+  createPaymentIntent,
+  handlePaymentConfirmation,
+} from "../controllers/payment.controller";
+import { authMiddleware } from "../middleware/auth";
 
 const router = express.Router();
 
-router.post('/checkout', authMiddleware, createCheckoutSession);
-router.get('/bank-details', authMiddleware, getBankDetails);
-router.post('/bank-details', authMiddleware, addBankDetail);
-router.post('/callback', handlePaymentCallback);
-router.post('/update-bank-details', authMiddleware, updateBankDetails);
-router.post('/create-payment-intent', authMiddleware, createPaymentIntent);
+router.post("/checkout", authMiddleware, createCheckoutSession);
+router.post(
+  "/handle-payment-confirmation",
+  authMiddleware,
+  handlePaymentConfirmation
+);
+router.get("/bank-details", authMiddleware, getBankDetails);
+router.post("/bank-details", authMiddleware, addBankDetail);
+// router.post('/callback', handlePaymentCallback);
+router.post("/update-bank-details", authMiddleware, updateBankDetails);
+router.post("/create-payment-intent", authMiddleware, createPaymentIntent);
 
-export default router; 
+export default router;
