92b81973824a3aef2ded7a6c820462322b128a8c
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2025-04-02 13:35:40 +0530

    UPdated rate limiter

--- api/src/index.ts
+++ api/src/index.ts
@@ -46,7 +46,7 @@ app.use("/images", express.static(path.join(__dirname, "../public/images")));
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 3 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
 });
 app.use(limiter);
