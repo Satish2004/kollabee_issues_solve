26ce48fa0f274c5d9f91f1a1c36d616d3cdf26a6
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2025-04-30 10:14:20 +0530

    Feat : caching issue after logout

--- frontend/app/(protected)/(buyer)/buyer/layout.tsx
+++ frontend/app/(protected)/(buyer)/buyer/layout.tsx
@@ -1,17 +1,16 @@
 "use client";
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { IntroTour } from '@/components/tour/IntroTour';
-import { BuyerSidebar } from '../../../../components/buyer/buyer-sidebar';
-import  BuyerLayoutHeader  from '../../../../components/buyer/buyer-layout-header';
-import { CheckoutProvider } from '@/contexts/checkout-context';
-
+import { IntroTour } from "@/components/tour/IntroTour";
+import { BuyerSidebar } from "../../../../components/buyer/buyer-sidebar";
+import BuyerLayoutHeader from "../../../../components/buyer/buyer-layout-header";
+import { CheckoutProvider } from "@/contexts/checkout-context";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   useEffect(() => {
-    if(!localStorage.getItem("token")){
+    if (!localStorage.getItem("kollabee_token")) {
       router.push("/login");
     }
   }, []);
@@ -26,13 +25,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       {/*This is Scrollable Content Area, currently it's keeping the header */}
       <div className="flex-1 flex flex-col overflow-y-auto">
         <CheckoutProvider>
-        <div className="sticky top-0 z-10 bg-gray-100">
-          <BuyerLayoutHeader/>
-        </div>
+          <div className="sticky top-0 z-10 bg-gray-100">
+            <BuyerLayoutHeader />
+          </div>
 
-        <main className="flex-1 px-4 py-3 bg-gray-100">
-          {children}
-        </main>
+          <main className="flex-1 px-4 py-3 bg-gray-100">{children}</main>
         </CheckoutProvider>
       </div>
 
