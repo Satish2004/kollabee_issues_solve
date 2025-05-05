4e5e5e9d55b5ae7b091d0416a4b957c1a03bd7c9
SurajjBhardwaj <pandeyyysuraj@gmail.com>
2025-05-05 12:22:59 +0530

    Prevent navigation to login page, redirecting to home page

--- frontend/app/(protected)/(buyer)/buyer/layout.tsx
+++ frontend/app/(protected)/(buyer)/buyer/layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (!localStorage.getItem("kollabee_token")) {
-      router.push("/login");
+      router.push("/");
     }
   }, []);
 
