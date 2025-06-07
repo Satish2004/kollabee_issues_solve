78e216ad86b89931935850b31f593a6557741ac8
surajjbhardwaj <pandeyyysuraj@gmail.com>
2025-06-07 07:27:18 +0530

    Singup up form working fine =

--- frontend/app/(protected)/(buyer)/buyer/settings/page.tsx
+++ frontend/app/(protected)/(buyer)/buyer/settings/page.tsx
@@ -341,7 +341,7 @@ const Settings: React.FC = () => {
 
   const getUser = useCallback(async () => {
     const response: any = await profileApi.getCurrentUser();
-    console.log("User", response);
+    // console.log("User", response);
     setFormData(response);
   }, []);
 
